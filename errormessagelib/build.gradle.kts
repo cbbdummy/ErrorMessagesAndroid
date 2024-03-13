99fe47077a73d6b39288499e378caec303757495
ChetanBelsare <chetan.belsare@intelliswift.com>
2024-03-13 20:03:23 +0530

    Update publish version

--- errormessagelib/build.gradle.kts
+++ errormessagelib/build.gradle.kts
@@ -65,7 +65,7 @@ publishing {
         register<MavenPublication>("release") {
             groupId = "errormessagelib"
             artifactId = "errormessagelib"
-            version = "1.0.4"
+            version = "1.0.5"
 
             afterEvaluate {
                 from(components["release"])

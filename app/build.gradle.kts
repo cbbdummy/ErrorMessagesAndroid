0ac0b0c2fd8438180443fb9128604d3d42fdad00
ChetanBelsare <chetan.belsare@intelliswift.com>
2024-03-13 20:07:30 +0530

    Update java version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -30,8 +30,8 @@ android {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
     }
-    kotlin {
-        jvmToolchain(17)
+    kotlinOptions {
+        jvmTarget = "17"
     }
     buildFeatures {
         viewBinding = true
